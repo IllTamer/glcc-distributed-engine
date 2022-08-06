a80630146e3723a27ca0e5116bf64f4c78bbd339
IllTamer <765743073@qq.com>
2022-08-06 14:05:06 +0800

    Add table `jianmu_engine_future`

--- api/src/main/resources/database/Tables.sql
+++ api/src/main/resources/database/Tables.sql
@@ -21,4 +21,22 @@ CREATE TABLE `jianmu_engine_task` (
       `start_time` datetime NOT NULL,
       `end_time` datetime NOT NULL,
       PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `jianmu_engine_future` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `uuid` varchar(45) NOT NULL,
+    `transaction_id` bigint NOT NULL,
+    `type` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+    `priority` int NOT NULL,
+    `cron` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+    `script` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `start_time` datetime NOT NULL,
+    PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB AUTO_INCREMENT=1
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_0900_ai_ci COMMENT ='计划任务表';
