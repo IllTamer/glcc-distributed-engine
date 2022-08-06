034c4c6d68a060fadbb2f023f690ccc7acb93d98
IllTamer <765743073@qq.com>
2022-08-06 14:03:46 +0800

    Add MyBatis-Plus 分页插件

---
+++ api/src/main/java/dev/jianmu/engine/api/config/MyBatisPlusConfiguration.java
@@ -0,0 +1,19 @@
+package dev.jianmu.engine.api.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisPlusConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor getInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
